adeb0cddc4569ed89b74bab9cf841dca4f37b915
Danson Lino <danson.lino@student.moringaschool.com>
2023-10-17 03:55:44 +0300

    let's commit

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+// Multiply
+let num1 = 2;
+let num2 = 31;
+let multiply = num1*num2;
+console.log(multiply); 
+// Result, 62
 
-// Write your code here
+//  VARIABLE Random
+let random = Math.floor(Math.random()*
+(Number.MAX_SAFE_INTEGER -1))+1 ;
+console.log(random);
+
+// VARIABLE MOD
+let num3 = 14;
+let num4 = 5;
+let mod = numb3%num4;
+console.log(mod);
+
+// Highest number
+let number = [10,6,15,20,12,5];
+let max = Math.max(...numbers);
+console.log(max);
+
